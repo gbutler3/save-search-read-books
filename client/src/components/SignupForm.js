2a4bdee528ed36dfc979677599ae7a4345f976d5
grace butler <gmx.butler@gmail.com>
2021-09-30 13:03:02 -0500

    added mutations.js and queries.js, also added instructions in each file in the front end from the readme just for ease of knowing what to do

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -1,3 +1,7 @@
+//TODO: * `SignupForm.js`
+  //Replace the `addUser()` functionality imported from the `API` file with 
+  //the `ADD_USER` mutation functionality.
+
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 
---
+++ client/src/utils/mutations.js
@@ -0,0 +1,5 @@
+// TODO: * `mutations.js`:
+// 	`LOGIN_USER` will execute the `loginUser` mutation set up using Apollo Server.
+// 	`ADD_USER` will execute the `addUser` mutation.
+// 	`SAVE_BOOK` will execute the `saveBook` mutation.
+// 	`REMOVE_BOOK` will execute the `removeBook` mutation.
