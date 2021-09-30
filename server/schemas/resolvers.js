fbb8420a65d98bc822f9348d5150279d2a81ed65
grace butler <gmx.butler@gmail.com>
2021-09-30 11:33:57 -0500

    realized instructions pointed to user-controller.js. So worked on resolvers.js, and also added typeDefs.js working off of the instructions and using past examples

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -5,19 +5,9 @@ const { signToken } = require('../utils/auth');
 //we want to get all users and books?
 const resolvers = {
   Query: {
-    users: async () => {
-      return User.find().populate('books');
-    },
-    user: async (parent, { username }) => {
-      return User.findOne({ username }).populate('books');
-    },
-    books: async (parent, { username }) => {
-      const params = username ? { username } : {};
-      return book.find(params).sort({ createdAt: -1 });
-    },
     me: async (parent, args, context) => {
       if (context.user) {
-        return User.findOne({ _id: context.user._id }).populate('book');
+        return User.findOne({ _id: context.user._id }).populate('user');
       }
       throw new AuthenticationError('You need to be logged in!');
     },
@@ -45,28 +35,30 @@ const resolvers = {
     },
     saveBook: async (parent, { newbook }, context) => {
       if (context.user) {
-        const savebook = await User.findByIdAndUpdate(
+        const updateUser = await User.findByIdAndUpdate(
           {_id: context.user._id},
-          {$addToSet: {savedbooks: savedbook._id}}, //using add b/c it is used in examples
+          {$addToSet: {savedbook: newbook}}, //using add b/c it is used in examples
           //addtoSet vs pull; 
           //$addToSet doesn't add the item to the given field if it already contains it
           //$push will add the given object to field whether it exists or not
           {new: true}
           );
-        return savedbook;
+        return updateUser;
       }
       throw new AuthenticationError('You need to be logged in!');
     },
     removeBook: async (parent, { bookId }, context) => {
       if (context.user) {
-        const savebook = await User.findByIdAndUpdate(
+        const updateUser = await User.findByIdAndUpdate(
           {_id: context.user._id},
-          {$pull: {savedbooks: savedbook._id}},
+          {$pull: {savedbooks: bookId}},
           {new: true}
           );
-        return savedbook;
+        return updateUser;
       }
       throw new AuthenticationError('You need to be logged in!');
     },
   }
 }
+
+module.exports = resolvers;
