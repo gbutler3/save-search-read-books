93932a0f1b64fd0a1495ac1577eb8569b65600ee
grace butler <gmx.butler@gmail.com>
2021-09-30 13:10:54 -0500

    working on queries client side

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,2 +1,23 @@
 // TODO: *`queries.js`: 
 //This will hold the query `GET_ME`, which will execute the `me` query set up using Apollo Server.
+
+import { gql } from '@apollo/client';
+
+//reference typeDefs on serverside
+export const GET_ME = gql `
+  query me{
+    me {
+      _id
+      username
+      email
+      bookCount
+      savedBooks{
+        bookId
+        authors
+        description
+        title
+        image
+        link
+      }
+    }
+  }`;
