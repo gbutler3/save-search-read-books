ab497538bccde525db8fd8b131ce4516b36a2fa5
grace butler <gmx.butler@gmail.com>
2021-09-30 13:42:07 -0500

    working on mutations.js in client side

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,5 +1,76 @@
-// TODO: * `mutations.js`:
-// 	`LOGIN_USER` will execute the `loginUser` mutation set up using Apollo Server.
-// 	`ADD_USER` will execute the `addUser` mutation.
-// 	`SAVE_BOOK` will execute the `saveBook` mutation.
-// 	`REMOVE_BOOK` will execute the `removeBook` mutation.
+import { gql } from '@apollo/client';
+
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!){
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+         # type user which has:
+        _id
+        username
+        email
+        bookCount
+        savedBooks{
+          # savedBooks: [book] 
+          bookId
+          authors
+          description
+          title
+          image
+          link
+        }
+      }
+    }
+  }
+`;
+
+export const SAVE_BOOK = gql`
+  mutation saveBook($newBook:BookInput!){
+    saveBook(newBook: $newBook){
+      # type User which has:
+      _id
+      username
+      email
+      savedBooks{
+        # savedBooks: [book] 
+        bookId
+        authors
+        description
+        title
+        image
+        link
+      }
+    }
+  }
+`;
+
+export const REMOVE_BOOK= gql `
+  mutation removeBook($bookId: ID!){
+    removeBook(bookId: $bookId){
+      _id
+      username
+      email
+      savedBooks{
+        # savedBooks: [book] 
+        bookId
+        authors
+        description
+        title
+        image
+        link
+      }
+    }
+  }
+`;
--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,4 +1,3 @@
-// TODO: *`queries.js`: 
 //This will hold the query `GET_ME`, which will execute the `me` query set up using Apollo Server.
 
 import { gql } from '@apollo/client';
