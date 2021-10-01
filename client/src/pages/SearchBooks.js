7b42a9c19d68cf5bbf97899415823ae6c6ecd218
grace butler <gmx.butler@gmail.com>
2021-10-01 14:19:13 -0500

    working on fixing error in console from searchbooks.js file

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -7,7 +7,7 @@ import { useMutation } from '@apollo/client';
 import { SAVE_BOOK } from '../utils/mutations';
 
 import Auth from '../utils/auth';
-import { saveBook, searchGoogleBooks } from '../utils/API';
+import { searchGoogleBooks } from '../utils/API';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
 
 const SearchBooks = () => {
@@ -30,11 +30,9 @@ const SearchBooks = () => {
   // create method to search for books and set state on form submit
   const handleFormSubmit = async (event) => {
     event.preventDefault();
-
     if (!searchInput) {
       return false;
     }
-
     try {
       const response = await searchGoogleBooks(searchInput);
 
@@ -63,7 +61,6 @@ const SearchBooks = () => {
   const handleSaveBook = async (bookId) => {
     // find the book in `searchedBooks` state by the matching id
     const bookToSave = searchedBooks.find((book) => book.bookId === bookId);
-
     // get token
     const token = Auth.loggedIn() ? Auth.getToken() : null;
 
@@ -72,12 +69,17 @@ const SearchBooks = () => {
     }
 
     try {
-      const response = await saveBook(bookToSave, token);
-
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
-
+      const { data } = await savebook({
+        variables: {newBook: 
+          {
+          ...bookToSave,
+          token
+          }
+        }
+      });
+      // if (!response.ok) {
+      //   throw new Error('something went wrong!');
+      // }
       // if book successfully saves to user's account, save book id to state
       setSavedBookIds([...savedBookIds, bookToSave.bookId]);
     } catch (err) {
