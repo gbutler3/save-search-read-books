c6bc0583f539d822fe77f1fdbd3de9da44500b5b
grace butler <gmx.butler@gmail.com>
2021-10-01 13:57:00 -0500

    working on changing login form to use the mutation functionality vs the loginUser() function

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -6,21 +6,34 @@
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 
-import { loginUser } from '../utils/API';
+import { useMutation } from '@apollo/client';
+import { LOGIN_USER } from '../utils/mutations';
+
+// import { loginUser } from '../utils/API';
 import Auth from '../utils/auth';
 
-const LoginForm = () => {
-  const [userFormData, setUserFormData] = useState({ email: '', password: '' });
+const LoginForm = (props) => {
+  const [userFormData, setFormState] = useState({ 
+    email: '',
+    password: '',
+    });
+  const [loginUser, {error, data}] = useMutation(LOGIN_USER);
+
   const [validated] = useState(false);
   const [showAlert, setShowAlert] = useState(false);
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
-    setUserFormData({ ...userFormData, [name]: value });
+    setFormState({
+      ...userFormData,
+      [name]: value,
+    });
   };
 
+  // submit form
   const handleFormSubmit = async (event) => {
     event.preventDefault();
+    console.log(userFormData)
 
     // check if form has everything (as per react-bootstrap docs)
     const form = event.currentTarget;
@@ -30,21 +43,22 @@ const LoginForm = () => {
     }
 
     try {
-      const response = await loginUser(userFormData);
-
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
-
-      const { token, user } = await response.json();
-      console.log(user);
-      Auth.login(token);
+      const { data } = await loginUser ({
+        variables: {...userFormData},
+      });
+      // if (!response.ok) {
+      //   throw new Error('something went wrong!');
+      // }
+      // const { token, user } = await response.json();
+      // console.log(user);
+      Auth.login(data.login.token);
     } catch (err) {
       console.error(err);
       setShowAlert(true);
     }
 
-    setUserFormData({
+    // clear form values
+    setFormState({
       username: '',
       email: '',
       password: '',
