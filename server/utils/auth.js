69e7cc6ed1bddaf0e26a87712c09a7e242000b42
grace butler <gmx.butler@gmail.com>
2021-10-03 15:10:23 -0500

    working on login and realized i didn't have context in the server file, so fixed that and auth.js file added so that token can be sent via req.body

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -7,8 +7,8 @@ const expiration = '2h';
 module.exports = {
   // function for our authenticated routes
   authMiddleware: function ({req}) {
-    // allows token to be sent via  req.query or headers
-    let token = req.query.token || req.headers.authorization;
+    // allows token to be sent via  req.query, req.body or headers now
+    let token = req.query.token || req.headers.authorization || req.body.token;
 
     // ["Bearer", "<tokenvalue>"] We split the token string into an array and return actual token
     if (req.headers.authorization) {
